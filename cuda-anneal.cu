bc0e59acc746bfafc3f6530c41081fbb6a8630f2
Moreno Marzolla <moreno.marzolla@unibo.it>
2023-01-29 14:00:30 +0100

    modifications

--- cuda-anneal.cu
+++ cuda-anneal.cu
@@ -203,7 +203,7 @@ Example:
 
 - Tommaso Toffoli, Norman Margolus, _Cellular Automata Machines: a new
   environment for modeling_, MIT Press, 1987, ISBN 9780262526319.
-  [PDF](https://people.csail.mit.edu/nhm/cam-book.pdf) from Normal
+  [PDF](https://people.csail.mit.edu/nhm/cam-book.pdf) from Norman
   Margolus home page.
 
 ## Files
