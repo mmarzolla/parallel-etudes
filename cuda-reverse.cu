30023f7b35c1f5b7bee1bbd0ce28ba09fa73a867
Moreno Marzolla <moreno.marzolla@unibo.it>
2023-11-30 13:15:23 +0100

    modifications

--- cuda-reverse.cu
+++ cuda-reverse.cu
@@ -25,28 +25,61 @@
 
 Write a program that reverses an array `v[]` of length $n$, i.e.,
 exchanges `v[0]` and `v[n-1]`, `v[1]` and `v[n-2]` and so on. You
-should write two versions of the program:
+should write the following functions:
 
-1. the first version ('reverse()') reverses an array `in[]` into a
-   different array `out[]` (the input is not modified). Assume that
-   `in[]` and `out[]` reside on non-overlapping memory blocks.
+1. `reverse()` reverses an array `in[]` into a different array `out[]`
+   (the input is not modified). Assume that `in[]` and `out[]` reside
+   on non-overlapping memory blocks.
 
-2. The second version (`inplace_reverse()`) modifies the input array
-   `in[]` using $O(1)$ additional storage; therefore, you are not
-   allowed to allocate a temporary output vector.
+2. `inplace_reverse()` reverses the array `in[]` "in place", i.e.,
+   exchanging elements using $O(1)$ additional storage; therefore, you
+   are not allowed to allocate a temporary output vector.
 
-The file [cuda-reverse.cu](cuda-reverse.cu) provides a CPU-based
-implementation of `reverse()` and `inplace_reverse()`.  Modify the
-functions to use of the GPU.
+The file [cuda-reverse.cu](cuda-reverse.cu) provides a serial
+implementation of `reverse()` and `inplace_reverse()`. Your goal is to
+odify the functions to use of the GPU, defining any additional kernel
+that is required.
 
-**Hint:** `reverse()` can be easily transformed into a kernel executed
-by $n$ CUDA threads (one for each array element). Each thread copies
-one element from `in[]` to `out[]`. Use as many one-dimensional
-_thread blocks_ as needed to have at least $n$ threads.
-`inplace_reverse()` can be transformed into a kernel that uses
-$\lfloor n/2 \rfloor$ CUDA threads (note the rounding): each thread
-swaps an element on the first half of `in[]` with the corresponding
-element on the second half.
+## Hints
+
+`reverse()` can be parallelized by launching $n$ CUDA threads; each
+thread copies a single element from the input to the output
+array. Since the array size $n$ can be large, you should create as
+many one-dimensional _thread blocks_ as needed to have at least $n$
+threads. Have a look at the lecture notes on how to do this.
+
+`inplace_reverse()` can be parallelized by launching $\lfloor n/2
+\rfloor$ CUDA threads (note the rounding): each thread swaps an
+element on the first half of `in[]` with the corresponding element on
+the second half.
+
+To map threads to array elements it is possible to use the expression:
+
+```C
+const int idx = threadIdx.x + blockIdx.x * blockDim.x;
+```
+
+In both cases the program might create more threads than actually
+needed; special care should be made to ensure that the extra threads
+do nothing, e.g., using
+
+```C
+if (idx < n) {
+  \/\* body \*\/
+}
+\/\* else do nothing \*\/
+```
+
+for `reverse()`, and
+
+```C
+if (idx < n/2) {
+  \/\* body \*\/
+}
+\/\* else do nothing \*\/
+```
+
+for `inplace_reverse()`.
 
 To compile:
 
