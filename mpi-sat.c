8759441092f5abe7e8c2294a88ab9af9716f20dc
Moreno Marzolla <moreno.marzolla@unibo.it>
2024-11-29 20:40:34 +0100

    modifications

--- mpi-sat.c
+++ mpi-sat.c
@@ -22,34 +22,65 @@
 /***
 % HPC - Brute-force SAT solver
 % [Moreno Marzolla](https://www.moreno.marzolla.name/)
-% Last updated: 2024-09-29
+% Last updated: 2024-11-29
 
-A SAT problem is represented as a pair of integer arrays `x[]` and
+The Boolean Satisfability Problem (SAT Problem) asks whether there
+exists an assignment of boolean variables $x_1, \ldots, x_n$ that
+makes a given boolean expression true. In this exercise we consider
+Boolean expressions in _Conjunctive Normal Form_ (CNF), which means
+that the expression is the conjunction (logical _and_) of clauses
+which in turn are the disjunction (logical _or_) of (possibly negated)
+variables. For example, the following expression with variables $x_1,
+x_2, x_3, x_4$ is in CNF:
+
+$$
+(x_1 \vee \neg x_2 \vee x_4) \wedge
+(\neg x_2 \vee x_3) \wedge
+(\neg x_1 \vee x_2 \vee \neg x_4)
+$$
+
+The expression above has three _clauses_, namely, $(x_1 \vee \neg x_2
+\vee x_4)$, $(\neg x_2 \vee x_3)$ and $(\neg x_1 \vee x_2 \vee \neg
+x_4)$.
+
+An assignment of $n$ Boolean variables $x_1, x_2, \ldots, x_n$ can be
+encoded as a binary value $v = (x_n \cdots x_2 x_1)$. In this exercise
+we develop a parallel SAT solver that tries all $2^n$ assignments, and
+counts the number of solutions, i.e., the number of assignments that
+make the Boolean expression true.
+
+To make the approach less inefficient, it is important to use a
+suitable representation of the Boolean expression, so that it can be
+evaluated quickly. To this aim, let us assume that there are at most
+30 Boolean variables, so that an assignment can be encoded as an `int`
+value without overflow (see the code).
+
+We represent a SAT problem as a pair of integer arrays `x[]` and
 `nx[]` of length `nclauses`. `x[i]` and `nx[i]` represents the _i_-th
-clause as follows. The binary digits of `x[i]` are the coefficients of
-the terms that are _true_ in the _i_-th clause; the binary digits of
-`nx[i]` are the literals that are _false_ in the _i_-th clause.  For
-example, if the _i_-th clause is:
+clause as follows. The binary digits of `x[i]` correspond to the
+variables that are not negated in the _i_-th clause; the binary digits
+of `nx[i]` correspond to the variables that are negated in the _i_-th
+clause.
+
+For example, if the _i_-th clause is:
 
 $$
 x_1 \vee x_3 \vee \neg x_4 \vee \neg x_5 \vee x_7
 $$
 
-then it is represented as:
+then $x_1, x_3, x_7$ are not negated, and therefore the first, third
+and seventh bit of `x[i]` will be set to one (the first bit is the
+rightmost one); similarly, $x_4, x_5$ are negated, and therefore the
+fourth and fifth bits of `nx[i]` will be set to one:
 
          x[i] = 1000101_2 = 69_10
         nx[i] = 0011000_2 = 24_10
 
 (here _2 and _10 denote base two and ten, respectively). Note that
-literals are indexed starting from one; the coefficient of $x_1$ is
-encoded in the rightmost bit. The representation above allows at most
-`8*sizeof(int)-1` literals using the `int` datatype, which increases
-to `8*sizeof(uint32_t)` using `uint32_t`.
-
-The representation has the advantage that evaluating a term can be
-done in constant time. Let the binary digits of `v` represent the
-assignment of values to literals.  Then, the term is true if and only
-if
+literals are indexed starting from one. This representation has the
+advantage that evaluating a clause can be done in constant time. Let
+the binary digits of an intecer `v` represent the assignment of values
+to literals.  Then, clause _i_ is true if and only if
 
         (v & x[i]) | (~v & nx[i])
 
@@ -58,15 +89,16 @@ is nonzero.
 For example, let us consider the assignment
 
 $$
-x_1 = x_3 = x_4 = x_5 = 0 \\
+x_1 = x_3 = x_4 = x_5 = 0 \qquad
 x_2 = x_6 = x_7 = 1
 $$
 
 This assignment can be encoded as an integer `v` whose binary
 representation is:
 
+$$
         v = (x_7 x_6 x_5 x_4 x_3 x_2 x_1)_2 = 1100010_2
-
+$$
 and using the values of `x` and `nx` above, we have:
 
 ```
@@ -101,9 +133,7 @@ To execute:
 #include <assert.h>
 #include <mpi.h>
 
-/* MAXLITERALS must be at most (bit width of int) - 2 */
-#define MAXLITERALS 30
-/* MAXCLAUSES must be a power of two */
+#define MAXLITERALS (8*sizeof(int) - 2)
 #define MAXCLAUSES 512
 
 typedef struct {
@@ -201,61 +231,54 @@ void pretty_print( const problem_t *p )
  */
 void load_dimacs( FILE *f, problem_t *p )
 {
-    int result;
-    int c, l, val;
-    int prob_c, prob_l;
+    int ret, nc, nl;
 
     /* Clear all bitmasks */
-    for (c=0; c<MAXCLAUSES; c++) {
+    for (int c=0; c<MAXCLAUSES; c++) {
         p->x[c] = p->nx[c] = 0;
     }
-    p->nlit = -1;
-    c = l = 0;
-    /* From
-       <https://github.com/marijnheule/march-SAT-solver/blob/master/parser.c> */
-    do {
-        result = fscanf(f, " p cnf %i %i \n", &prob_l, &prob_c);
-        if ( result > 0 && result != EOF )
-            break;
-        result = fscanf(f, "%*s\n");
-    } while( result != 2 && result != EOF );
 
-    if ( prob_l > MAXLITERALS-1 ) {
-        fprintf(stderr, "FATAL: too many literals (%d); please set MAXLITERALS to at least %d\n", prob_l, prob_l+1);
+    do {
+        ret = fscanf(f, " p cnf %i %i \n", &nl, &nc);
+        if ( ret > 0 && ret != EOF )
+            break;
+        ret = fscanf(f, "%*s\n");
+    } while( ret != 2 && ret != EOF );
+
+    if ( nl > MAXLITERALS-1 ) {
+        fprintf(stderr, "FATAL: too many literals (%d); please set MAXLITERALS to at least %d\n", nl, nl+1);
         exit(EXIT_FAILURE);
     }
-    if ( prob_c > MAXCLAUSES-1 ) {
-        fprintf(stderr, "FATAL: too many clauses (%d); please set MAXCLAUSES to at least %d\n", prob_c, prob_c+1);
+    if ( nc > MAXCLAUSES-1 ) {
+        fprintf(stderr, "FATAL: too many clauses (%d); please set MAXCLAUSES to at least %d\n", nc, nc+1);
         exit(EXIT_FAILURE);
     }
-    while (fscanf(f, "%d", &val) == 1) {
-        if (val == 0) {
-            /* Check the previous clause for consistency */
-            assert( (p->x[c] & p->nx[c]) == 0 );
-            /* New clause */
-            l = 0;
-            c++;
-        } else {
-            /* New literal */
-            if (val > 0) {
-                p->x[c] |= (1 << (val-1));
-            } else {
-                p->nx[c] |= (1 << -(val+1));
+    p->nlit = -1; /* we compute the number of literals from those actually used in the file */
+    p->nclauses = nc;
+    for (int c=0; c<nc; c++) {
+        int val;
+        do {
+            ret = fscanf(f, "%d", &val);
+            assert(ret == 1);
+            if (val)  {
+                if (val > 0) {
+                    p->x[c] |= (1 << (val-1));
+                } else {
+                    p->nx[c] |= (1 << -(val+1));
+                }
+                p->nlit = max(p->nlit, abs(val));
             }
-            p->nlit = max(p->nlit, abs(val));
-            l++;
-        }
+        } while (val);
+        assert( (p->x[c] & p->nx[c]) == 0 );
     }
-    p->nclauses = c;
-    fprintf(stderr, "DIMACS CNF files: %d clauses, %d literals\n", c, p->nlit);
+    fprintf(stderr, "DIMACS CNF files: %d clauses, %d literals\n", nc, p->nlit);
 }
 
 int main( int argc, char *argv[] )
 {
     problem_t p;
 
-    assert(MAXLITERALS <= 8*sizeof(int)-1);
-    assert((MAXCLAUSES & (MAXCLAUSES-1)) == 0); /* "bit hack" to check whether MAXCLAUSES is a power of two */
+    assert(MAXLITERALS <= 8*sizeof(int)-2);
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
