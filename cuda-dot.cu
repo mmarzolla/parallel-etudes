ece3fa4ed3bcc3b51d6b0e074bcf69dc2681bfba
Moreno Marzolla <moreno.marzolla@unibo.it>
2025-11-13 22:43:12 +0100

    modifications

--- cuda-dot.cu
+++ cuda-dot.cu
@@ -2,7 +2,7 @@
  *
  * cuda-dot.cu - Dot product
  *
- * Copyright (C) 2017--2024 Moreno Marzolla
+ * Copyright (C) 2017--2025 Moreno Marzolla
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,47 +22,26 @@
 /***
 % HPC - Dot product
 % [Moreno Marzolla](https://www.unibo.it/sitoweb/moreno.marzolla)
-% Last updated: 2024-11-14
-
-## Familiarize with the environment
-
-The server has three identical GPUs (NVidia GeForce GTX 1070). The
-first one is used by default, although it is possible to select
-another GPU either programmatically (`cudaSetDevice(0)` uses the
-first GPU, `cudaSetDevice(1)` uses the second one, and so on), or
-using the environment variable `CUDA_VISIBLE_DEVICES`.
-
-For example
-
-        CUDA_VISIBLE_DEVICES=0 ./cuda-stencil1d
-
-runs `cuda-stencil1d` on the first GPU (default), while
-
-        CUDA_VISIBLE_DEVICES=1 ./cuda-stencil1d
-
-runs the program on the second GPU.
-
-Run `deviceQuery` from the command line to display the hardware
-features of the GPUs.
-
-## Scalar product
+% Last updated: 2025-11-13
 
 The program [cuda-dot.cu](cuda-dot.cu) computes the dot product of two
 arrays `x[]` and `y[]` of length $n$. Modify the program to use the
 GPU, by defining a suitable kernel and modifying the `dot()` function
-to use it. The dot product $s$ of two arrays `x[]` and `y[]` is defined as
+to use it.
+
+The dot product $s$ of two arrays `x[]` and `y[]` is:
 
 $$
 s = \sum_{i=0}^{n-1} x[i] \times y[i]
 $$
 
-In this exercise we implement a simple (although not efficient)
-approach where we use a _single_ block of _BLKDIM_ threads.  The
-algorithm works as follows:
+In this exercise we implement a simple (but not efficient) approach
+where we use _one_ block of _BLKDIM_ threads. The algorithm works as
+follows:
 
-1. The GPU executes a single 1D thread block containing _BLKDIM_
-   threads; use the maximum number of threads per block supported by
-   the hardware, which is _BLKDIM = 1024_.
+1. The GPU executes one thread block containing _BLKDIM_ threads; use
+   the maximum number of threads per block supported by the hardware,
+   which is _BLKDIM = 1024_.
 
 2. The block defines a float array `tmp[]` of length _BLKDIM_ in
    shared memory.
@@ -243,12 +222,12 @@ int main( int argc, char* argv[] )
 
     printf("Computing the dot product of %d elements...\n", n);
     result = dot(x, y, n);
-    printf("got=%f, expected=%f\n", result, expected);
 
     /* Check result */
     if ( fabs(result - expected) < 1e-5 ) {
         printf("Check OK\n");
     } else {
+        printf("got=%f, expected=%f\n", result, expected);
         printf("Check FAILED\n");
     }
 
